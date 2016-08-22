1d902f29f992a48b44838b5996ab9da941bfab19
crandmck <crandmck@yahoo.com>
2016-08-22 11:15:41 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # get-readmes
-Simple Node script to pull down module READMEs using the GitHub API
+Simple Node script to pull down module READMEs using the GitHub API.
+Primary use case is for use with Jekyll to include docs from external repositories.
+
+## Installation
+
+```
+npm install
+```
+
+## Use
+
+1. Create `repos.json` containing list of repos for which you want to pull down READMEs.  A sample is provided.
+1. Create directory, where you want to save the README files.  By default, the script
+uses `./readmes` directory.  To change, set the `DIRPATH` variable in the script.  The directory _must_ exist before running the script.
+1. To run the script:
+```
+node .
+```
+
+**NOTE**: This script accesses GitHub APIs without authorization.  Since it uses only
+"read" operations, no authorization is required; however, for unauthenticated requests, GitHub API rate limit is 60 requests per hour. For more information, see [https://developer.github.com/v3/#rate-limiting](https://developer.github.com/v3/#rate-limiting).
---
+++ index.js
@@ -0,0 +1,56 @@
+var GitHubApi = require("github");
+var github = new GitHubApi({
+  debug: true,
+  headers: { "Accept-Charset": "ISO-8859-1,utf-8"}
+});
+
+var DIRPATH = "readmes";
+
+writeReadme = function(org, module, path) {
+  var options = {};
+  options.user = org;
+  options.repo = module;
+
+  github.repos.getLatestRelease({ user: org, repo: module }, function(err, res) {
+    if(err) { // Use master release
+      console.log("For " + org + "/" + module + " -- No latest release ");
+
+    } else { // Use latest tagged release if it exists
+      console.log("For " + org + "/" + module  + " latest release is " + res.tag_name );
+      options.ref = res.tag_name;
+    }
+
+    github.repos.getReadme(options, function(err, res) {
+      if (err) {
+        console.log("ERROR! Cannot get README.")
+        console.log(err);
+
+      } else {
+        console.log("Getting README for " + JSON.stringify(options) );
+        writeMdFile(res.content, module);
+      }
+    })
+  });
+}
+
+writeMdFile = function(str, module) {
+  var file = __dirname + "/" + DIRPATH + "/" + module + ".md"
+  var s = new Buffer(str, 'base64').toString();
+  var fs = require('fs');
+  fs.writeFile(file, s, function(err) {
+      if(err) {
+        return console.log(err);
+      }
+  });
+}
+
+/*
+END OF FUNCTION DEFINITIONS.
+*/
+var repoList = require('./repos.json');
+//nRepos = repoList.repos.length;
+nRepos = 1;
+for (i=0; i < nRepos ; i++ ) {
+  console.log("org = " + repoList.repos[i].org  + " repo = " + repoList.repos[i].repoName )
+  writeReadme(repoList.repos[i].org, repoList.repos[i].repoName , DIRPATH)
+}
